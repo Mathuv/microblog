79e31fdb41ffd9198406350e51f8e7353dd39a8c
Mathu Mounasamy <mmounasamy@gmail.com>
2017-02-21 08:12:21 +1100

    disolay flashed message, redirect to index from login

--- app/views.py
+++ app/views.py
@@ -1,38 +1,50 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+# @app.route('/')
+# @app.route('/index')
+@app.route('/login', methods=['GET', 'POST'])
 
-def index():
-    user = {'nickname': 'Mathu'} # fake user
-    # return '''
-    # <html>
-    #     <head>
-    #         <title>Home Page</title>
-    #     </head>
-    #     <body>
-    #         <h1>Hello, ''' + user['nickname'] + '''</h1>
-    #     </body>
-    # </html>
-    # '''
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
 
-    posts = [ # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-     ]
-
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
-
-    # return render_template('index.html',
-    #                        user=user)
+# def index():
+#     user = {'nickname': 'Mathu'} # fake user
+#     # return '''
+#     # <html>
+#     #     <head>
+#     #         <title>Home Page</title>
+#     #     </head>
+#     #     <body>
+#     #         <h1>Hello, ''' + user['nickname'] + '''</h1>
+#     #     </body>
+#     # </html>
+#     # '''
+#
+#     posts = [ # fake array of posts
+#         {
+#             'author': {'nickname': 'John'},
+#             'body': 'Beautiful day in Portland!'
+#         },
+#         {
+#             'author': {'nickname': 'Susan'},
+#             'body': 'The Avengers movie was so cool!'
+#         }
+#      ]
+#
+#     return render_template('index.html',
+#                            title='Home',
+#                            user=user,
+#                            posts=posts)
+#
+#     # return render_template('index.html',
+#     #                        user=user)
